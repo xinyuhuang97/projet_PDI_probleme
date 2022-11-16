f2833b5e25a079a182f40457648a13880684e79c
xinyuhuang <xinyuhuang97@hotmail.com>
2022-11-16 10:16:29 +0100

    backup for resolution exacte

---
+++ resolution_exact.jl
@@ -0,0 +1,189 @@
+using JuMP
+using CPLEX
+include("read_data.jl")
+
+# Définition de constantes pour le statut de résolution du problème
+const OPTIMAL = JuMP.MathOptInterface.OPTIMAL
+const INFEASIBLE = JuMP.MathOptInterface.INFEASIBLE
+const UNBOUNDED = JuMP.MathOptInterface.DUAL_INFEASIBLE;
+
+
+function Resolution_exacte(filename)
+    file_type, nb_clients, general_info, clients_info, demand_info = Read_instance(filename)
+
+    nb_t=general_info[3] #nombre de periodes
+    u=general_info[4] #cout unitaire de production
+    f=general_info[5] #cout de setup de production
+    C=general_info[6] #capacite de production
+    Q=general_info[7] #capacite d'un vehicule
+    k=general_info[8] #nombre de vehicules
+    #m=k # Notation de l'article -> K={1....m}
+
+    h0=clients_info[0][3] #cout de stockage
+    L0=clients_info[0][4] #capacite de stockage pour le producteur
+    #L00=clients_info[1,5] #le stock intial pour le producteur
+    d=demand_info
+    M=[]
+    Mp=[]
+    for j in 1:nb_clients+1
+        push!(M,500000)
+        push!(Mp,500000)
+    end
+
+    ci0=Array{Float64}(undef, nb_clients+1)
+    cij=Array{Float64, 2}(undef, nb_clients+1, nb_clients+1)
+    if file_type=="A"
+        for i in 1:nb_clients+1
+            ci0[i]=floor(hypot((clients_info[i-1][1]-clients_info[0][1])^2 + (clients_info[i-1][2]-clients_info[0][2]))^2 +1/2)
+        end
+        for i in 1:nb_clients+1
+            for j in 1:nb_clients+1
+
+                cij[i,j]=floor(hypot((clients_info[i-1][1]-clients_info[j-1][1])^2 + (clients_info[i-1][2]-clients_info[j-1][2])^2) +1/2)
+            end
+        end
+    end
+    if file_type=="B"
+        mc=general_info[9]
+        for i in 1:nb_clients+1
+            ci0[i]=floor(mc*hypot((clients_info[i-1][1]-clients_info[0][1])^2 + (clients_info[i-1][2]-clients_info[0][2])^2 ))
+        end
+        for i in 1:nb_clients+1
+            for j in 1:nb_clients+1
+                cij[i,j]=mc*hypot((clients_info[i-1][1]-clients_info[j-1][1])^2 + (clients_info[i-1][2]-clients_info[j-1][2])^2 )
+            end
+        end
+    end
+
+    m = Model(CPLEX.Optimizer)
+
+    # L'ajout de variables
+
+    # production quantity in period t
+    @variable(m, p[1:nb_t],lower_bound = 0 ) # variable de production R+
+    # inventory at node i at the end of period t
+    @variable(m, i[1:nb_clients+1, 1:nb_t],lower_bound = 0 )#lower_bound = 0) #variable de stockage
+    # equal to 1 if there is production at the plant in period t, 0 otherwise
+    @variable(m, y[1:nb_t], Bin) # variable de lancement
+    # the number of vehicles leaving the plant in period t;
+    @variable(m, z0t[1:nb_t],lower_bound = 0) # variable de nb de vehicules partent a partir du producteur
+    # equal to 1 if customer i is visited in period t, 0 otherwise,
+    @variable(m, zit[1:nb_clients, 1:nb_t],Bin) # variable de visite
+    # if a vehicle travels directly from node i to node j in period t, 0 otherwise
+    @variable(m, x[1:nb_clients+1, 1:nb_clients+1, 1:nb_t], Bin) #variable binaire pour dire si un vehicule voyage de i a j a peride t.
+    # quantity delivered to customer i in period t
+    @variable(m, q[1:nb_clients, 1:nb_t],lower_bound = 0 )#lower_bound = 0) #variable d'approvisionnement
+    # load of a vehicle before making a delivery to customer i in period t.
+    @variable(m, w[1:nb_clients, 1:nb_t],lower_bound = 0)
+    # Fonction objective
+    @objective(m, Min, sum(u*p[t] + f*y[t] + sum(clients_info[ind-1][3]*i[ind,t]  for ind = 2:nb_clients+1 ) +  sum((ci0[ind]*x[ind,ind2,t] for ind2 = 1:nb_clients+1) for ind = 1:nb_clients+1) for t=1:nb_t))
+
+    # same as in Resolution_heuristique
+    # contraintes (1) pour dire "en stock à t-1 + production à t = produite pour tous les revendeurs + en stock à t
+    @constraint(m, clients_info[0][5]+p[1]==sum(q[ind-1,1] for ind = 2:nb_clients+1) +i[1,1])
+    for t in 2:nb_t
+        @constraint(m, i[1,t-1]+p[t]==sum(q[ind-1,t] for ind = 2:nb_clients+1) +i[1,t])
+    end
+
+    # same as in Resolution_heuristique
+    # contraintes (2) pour dire “L'equivalence : stock à t-1 +approvisionnement à t = demande+stock a l'instant t”
+    for ind in 2:nb_clients+1
+        @constraint(m, clients_info[ind-1][5] + q[ind-1,1]==d[ind-1][1]+i[ind,1])
+    end
+    for t in 2:nb_t
+        for ind in 2:nb_clients+1
+            @constraint(m, i[ind,t-1] + q[ind-1,t]==d[ind-1][t-1]+i[ind,t])
+        end
+    end
+
+    # same as in Resolution_heuristique
+    #contraintes (3) pour dire que la variable de production est à 0 si la variable de lancement est à 0
+    for t in 1:nb_t
+        @constraint(m,p[t]<=M[t]*y[t])
+    end
+
+    # same as in Resolution_heuristique
+    # contraintes (4) pour dire le stock du producteur est inferieur à son capacoté de stockage
+    @constraint(m,clients_info[0][5]<=L0)
+    for t in 2:nb_t
+        @constraint(m,i[1,t-1]<=L0)
+    end
+
+    # same as in Resolution_heuristique
+    # contraintes (5)(6) pour dire le stock du revendeurs est inferieur à son capacoté de stockage
+    for t in 1:nb_t
+        for ind in 2:nb_clients+1
+            Li=clients_info[ind-1][4]
+            if t==1
+                @constraint(m,clients_info[ind-1][5]+q[ind-1,t]<=Li)
+            else
+                @constraint(m,i[ind,t-1]+q[ind-1,t]<=Li)
+            end
+        end
+    end
+
+    # contraintes (7) La contrainte ajouté: la variable de visite est à 0 si quantité d'approvisionnement est à 0
+    for t in 1:nb_t
+        for ind in 2:nb_clients+1
+            @constraint(m,q[ind-1,t]<=Mp[t]*zit[ind-1,t])
+        end
+    end
+
+    # contraintes (8) pour dire une ville peut au plus être visité au plus par une voiture
+    for t in 1:nb_t
+        for ind in 2:nb_clients+1
+            @constraint(m,sum(x[ind,ind2,t] for ind2 = 2:(nb_clients+1))==zit[ind-1,t])
+        end
+    end
+
+    #contraintes (9) pour dire
+    for t in 1:nb_t
+        for ind in 2:nb_clients+1
+            @constraint(m,sum(x[ind,ind2,t] +x[ind2,ind,t] for ind2 = 2:(nb_clients+1) )==2*zit[ind-1,t])
+        end
+    end
+
+    #contraintes (10) pour dire
+    for t in 1:nb_t
+        @constraint(m,z0t[t]<=k)
+    end
+
+    #contraintes (11) pour dire
+    #=for t in 1:nb_t
+        for i in 2:nb_clients+1
+            for j in 2:nb_clients+1
+                @constraint(m,w[i-1,t]-w[j-1,t]>=q[i-1,t]-Mp[i][t]*(1-x[i,j,t]))
+            end
+        end
+    end=#
+
+    #contraintes (12) pour dire
+    for t in 1:nb_t
+        for i in 2:nb_clients+1
+            @constraint(m,0<=w[i-1,t])
+            @constraint(m,w[i-1,t]<=Q*zit[i-1,t])
+        end
+    end
+
+    #contraintes (13-16) donnees dans la definition de variables
+
+    optimize!(m)
+    println("Fin de la résolution du PLNE par le solveur")
+
+    status = termination_status(m)
+    println(status)
+    # un petit affichage sympathique
+    if status == JuMP.MathOptInterface.OPTIMAL
+        q_vals = value.(q)
+        #println(q_vals)
+        z_vals = value.(zit)
+
+        println("Temps de résolution :", solve_time(m))
+        return objective_value(m),q_vals,z_vals
+    else
+        println("Problème lors de la résolution")
+    end
+end
+
+#Resolution_exacte("PRP_instances/A_014_ABS75_15_2.prp")
+Resolution_exacte("PRP_instances/B_200_instance3.prp")
--- resolution_heur.jl
+++ resolution_heur.jl
@@ -8,12 +8,6 @@ const INFEASIBLE = JuMP.MathOptInterface.INFEASIBLE
 const UNBOUNDED = JuMP.MathOptInterface.DUAL_INFEASIBLE;
 
 
-
-
-
-
-
-
 function Resolution_heuristique(filename)
     file_type, nb_clients, general_info, clients_info, demand_info = Read_instance(filename)
 
@@ -23,9 +17,7 @@ function Resolution_heuristique(filename)
     C=general_info[6] #capacite de production
     Q=general_info[7] #capacite d'un vehicule
     k=general_info[8] #nombre de vehicules
-    if file_type=='B'
-        mc=general_info[9]
-    end
+
 
     h0=clients_info[0][3] #cout de stockage
     L0=clients_info[0][4] #capacite de stockage pour le producteur
@@ -34,6 +26,31 @@ function Resolution_heuristique(filename)
     M=[]
     for j in 1:nb_clients
         push!(M,500000)
+    end
+
+    ci0=Array{Float64}(undef, nb_clients+1)
+    cij=Array{Float64, 2}(undef, nb_clients+1, nb_clients+1)
+    if file_type=="A"
+        for i in 1:nb_clients+1
+            ci0[i]=floor(hypot((clients_info[i-1][1]-clients_info[0][1])^2 + (clients_info[i-1][2]-clients_info[0][2]))^2 +1/2)
+        end
+        for i in 1:nb_clients+1
+            for j in 1:nb_clients+1
+
+                cij[i,j]=floor(hypot((clients_info[i-1][1]-clients_info[j-1][1])^2 + (clients_info[i-1][2]-clients_info[j-1][2])^2) +1/2)
+            end
+        end
+    end
+    if file_type=="B"
+        mc=general_info[9]
+        for i in 1:nb_clients+1
+            ci0[i]=floor(mc*hypot((clients_info[i-1][1]-clients_info[0][1])^2 + (clients_info[i-1][2]-clients_info[0][2])^2 ))
+        end
+        for i in 1:nb_clients+1
+            for j in 1:nb_clients+1
+                cij[i,j]=mc*hypot((clients_info[i-1][1]-clients_info[j-1][1])^2 + (clients_info[i-1][2]-clients_info[j-1][2])^2 )
+            end
+        end
     end
     function PLNE_LSP()#filename)
         #=
@@ -55,27 +72,24 @@ function Resolution_heuristique(filename)
         #d=demand_info
 
     	m = Model(CPLEX.Optimizer)
-        #print(nb_clients,nb_t)
-        #println(nb_clients, general_info, clients_info, demand_info)
-        @variable(m, p[1:nb_t],lower_bound = 0 )#lower_bound = 0 )  # variable de production R+
+
+        # L'ajout de variables
+        @variable(m, p[1:nb_t],lower_bound = 0 ) # variable de production R+
         @variable(m, y[1:nb_t], Bin) # variable de lancement
         @variable(m, i[1:nb_clients+1, 1:nb_t],lower_bound = 0 )#lower_bound = 0) #variable de stockage
         @variable(m, q[1:nb_clients, 1:nb_t],lower_bound = 0 )#lower_bound = 0) #variable d'approvisionnement
+        @variable(m, z[1:nb_clients+1, 1:nb_t],Bin) # variable de visite
 
-        #sum(u*p[t] + f*y[t] + sum(h[i]*i[ind][t] for ind = 2:nb_clients+1) for t = 1:nb_t )
-        # h[i]=clients_info[ind][3] sum(clients_info[ind][3]*i[ind][t] for ind = 2:nb_clients+1)
-        #
-        #print(clients_info[0])
-        @objective(m, Min, sum(u*p[t] + f*y[t] + sum(clients_info[ind-1][3]*i[ind,t] for ind = 2:nb_clients+1 ) for t = 1:nb_t) )
+        # Fonction objective
+        @objective(m, Min, sum(u*p[t] + f*y[t] + sum(clients_info[ind-1][3]*i[ind,t]  for ind = 2:nb_clients+1 ) +  sum(2*ci0[ind]*z[ind,t] for ind = 1:nb_clients+1 ) for t = 1:nb_t) )
 
-
-        # contraintes (1) pour dire " en stock à t-1 + production à t = produite pour tous les revendeurs + en stock à t
+        # contraintes (1) pour dire "en stock à t-1 + production à t = produite pour tous les revendeurs + en stock à t
         @constraint(m, clients_info[0][5]+p[1]==sum(q[ind-1,1] for ind = 2:nb_clients+1) +i[1,1])
         for t in 2:nb_t
             @constraint(m, i[1,t-1]+p[t]==sum(q[ind-1,t] for ind = 2:nb_clients+1) +i[1,t])
         end
 
-        # contraintes (2) pour dire
+        # contraintes (2) pour dire “L'equivalence : stock à t-1 +approvisionnement à t = demande+stock a l'instant t”
         for ind in 2:nb_clients+1
             @constraint(m, clients_info[ind-1][5] + q[ind-1,1]==d[ind-1][1]+i[ind,1])
         end
@@ -84,16 +98,19 @@ function Resolution_heuristique(filename)
                 @constraint(m, i[ind,t-1] + q[ind-1,t]==d[ind-1][t-1]+i[ind,t])
             end
         end
-        #contraintes (3) pour dire
+
+        #contraintes (3) pour dire que la variable de production est à 0 si la variable de lancement est à 0
         for t in 1:nb_t
             @constraint(m,p[t]<=M[t]*y[t])
         end
-        # contraintes (4) pour dire
+
+        # contraintes (4) pour dire le stock du producteur est inferieur à son capacoté de stockage
         @constraint(m,clients_info[0][5]<=L0)
         for t in 2:nb_t
             @constraint(m,i[1,t-1]<=L0)
         end
-        # contraintes (5) pour dire
+
+        # contraintes (5) pour dire le stock du revendeurs est inferieur à son capacoté de stockage
         for t in 1:nb_t
             for ind in 2:nb_clients+1
                 Li=clients_info[ind-1][4]
@@ -104,74 +121,79 @@ function Resolution_heuristique(filename)
                 end
             end
         end
-    	print(m)
-    	println()
+        # contraintes (6) La contrainte ajouté: la variable de visite est à 0 si quantité d'approvisionnement est à 0
+        for t in 1:nb_t
+            for ind in 2:nb_clients+1
+                @constraint(m,z[ind,t]<=M[t]*q[ind-1,t])
+            end
+        end
 
-    	println("Résolution du PLNE par le solveur")
+    	#print(m)
+    	#println()
+
+    	#println("Résolution du PLNE par le solveur")
     	optimize!(m)
-       	println("Fin de la résolution du PLNE par le solveur")
+       	#println("Fin de la résolution du PLNE par le solveur")
 
     	status = termination_status(m)
-        println(status)
+        #println(status)
     	# un petit affichage sympathique
     	if status == JuMP.MathOptInterface.OPTIMAL
-    		#println("Valeur optimale = ", objective_value(m))
-    		#println("Solution primale optimale :")
             q_vals = value.(q)
-            println(q_vals)
-            println(size(q_vals))
-            i_vals = value.(i)
-            println(i_vals)
+            #println(q_vals)
+            z_vals = value.(z)
+
     		println("Temps de résolution :", solve_time(m))
-            return q_vals
+            return objective_value(m),q_vals,z_vals
     	else
     		println("Problème lors de la résolution")
     	end
-        #print("return",q_vals[1][1])
-        #print("return",q_vals[1])
-
-
     end
+
     function VRP_heurisitque_Binpacking(t)
         #= Principe :
             1) remplir les vehicules de facons gluton
             2) Forme un circuit de mainiere gloutonne
         =#
-
         #Q=general_info[7] #capacite d'un vehicule
         #k=general_info[8] #nombre de vehicules
-        cpt_vehi=1 #compteur de vehicule
-        bool_end=false #boolean pour detercter si on a bien traité tous les villes
+        cpt_vehi=1 # compteur de vehicule
+        bool_end=false # boolean pour detercter si on a bien traité tous les villes
         charge_actuel=0
-        # quantite d'approvisionnement
-        q=q_vals
+        # Creation d'une dictionnaire de partition donc chaque indice corresponde a une tournee d'un vehicule
         partition=Dict()
         for i in 1:nb_clients
-            #println(i,"_",t,q_vals)
-            #print(q_vals,q_vals[i][t])
             qi=q_vals[i,t]
+            # Tester si la quantite d'approvisionnement est a 0
             if isapprox(qi, 0.0; atol = 1e-8)
                 continue
             end
+            #=(on suppose qu'il n'existe pas de quantite d'approvisionnement superieur au capacite de vehicule)
+            Si le premier voiture -> nouvel indice dans la dictionnaire, l'ajout dans la liste corresponde a l'indice
+            Si la capacite est depasse -> nouvel indice dans la dictionnaire, l'ajout dans la liste corresponde a l'indice
+            Sinon, l'ajout dans la liste directement
+            =#
             if charge_actuel==0
                 charge_actuel=qi
                 partition[cpt_vehi]=Any[]
                 push!(partition[cpt_vehi],i)
             elseif charge_actuel+qi>Q
                 cpt_vehi+=1
-                # if cpt_vehi>k ? si depasse le nombre de vehicules qu'on possede
                 charge_actuel=qi
-                partition[cpt_vehi]=[i]
+                partition[cpt_vehi]=Any[]
+                push!(partition[cpt_vehi],i)
+                #partition[cpt_vehi]=[i]
             else
                 charge_actuel+=qi
                 push!(partition[cpt_vehi],i)
             end
         end
+
+        #dictionnaire contenant pour chaque tournee l'ordre de tournage
         tsp_partition=Dict()
         for i in 1:cpt_vehi
-            smallest_indice=-1 # l'indice plus proche du point de depart
-            #cherche le plus proche ville
-            #println(partition)
+            #Procedure de trouve le plus proche voisin
+            smallest_indice=-1
             lg=length(partition[i])
             ordre=Array{Int}(undef,lg)
             index=-1
@@ -179,15 +201,11 @@ function Resolution_heuristique(filename)
             index_ville=true
             index_min=-1
             while index_ville==true
-                #println(index_ville, ordre)
                 if index==-1
-                    #println(partition[i])
                     index=partition[i][1]
-                    #ordre[1]=index
                 else
                     dis_min=0
-
-                    for index2 in partition[i]# a modifier
+                    for index2 in partition[i]
                         if index !=index2
                             dist= (clients_info[index][1]-clients_info[index2][1])^2 + (clients_info[index][2]-clients_info[index2][2])^2
                             if dis_min==0
@@ -201,33 +219,51 @@ function Resolution_heuristique(filename)
                         end
                     end
                 end
-                #println(index)
-                #println(partition[i])
                 filter!(e->e≠index,partition[i])
-                #println(partition[i])
                 if index_min!=-1
                     index=index_min
                 end
                 ordre[cpt]=index
                 cpt+=1
-
-
-                #println("ordre",ordre)
-                #println(length(partition[i]))
                 if length(partition[i])<=1
                     index_ville=false
                 end
-                #println(index_ville, ordre)
             end
             tsp_partition[i]=ordre
         end
-        return tsp_partition
+        println(tsp_partition)
+        somme_sc_new=0
+        for (key, value) in tsp_partition
+            for j in 1:length(value)-1
+                #println(j)
+                #println("value",value[j])
+                #println(cij[value[j],value[j+1]])
+                somme_sc_new+=cij[value[j],value[j+1]]
+            end
+        end
+        #somme_sc_new=sum(( cij[value[j],value[j+1]] for j in 1:length(value)-1) for (key, value) in tsp_partition)
+        #diff=somme_sc_new-somme_sc_old
+        return tsp_partition,somme_sc_new
     end
-    q_vals=PLNE_LSP()
+    # Corp du code
+    val_objective,q_vals,z_vals=PLNE_LSP()
+    somme_sc_new=0
     for t in 1:nb_t
-        println(t)
-        println(VRP_heurisitque_Binpacking(t))
+        println("Temps",t)
+        tsp_partition,somme=VRP_heurisitque_Binpacking(t)
+        somme_sc_new+=somme
+        println(tsp_partition)
     end
+    somme_sc_old=0
+    for ind = 1:nb_clients+1
+        for t = 1:nb_t
+            somme_sc_old+=2*ci0[ind]*z_vals[ind,t]
+        end
+    end
+    #somme_sc_old=sum( (2*ci0[ind]*z_vals[ind,t] for t = 1:nb_t) for ind = 1:nb_clients+1)
+    println("Avant mise a jour: ",val_objective)
+    val_objective+=somme_sc_new-somme_sc_old
+    println("Apres mise a jour: ",val_objective)
 end
 
 #println(PLNE_LSP("PRP_instances/A_014_ABS75_15_2.prp"))
